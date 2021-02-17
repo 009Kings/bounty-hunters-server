175d6669367a73f940309dc1a64b913f3889cf08
Gavin Callander <gavincallander@MacBook-Pro.local>
2021-02-17 13:54:10 -0500

    get one, post and put done. almost there troops

--- controllers/bounties.js
+++ controllers/bounties.js
@@ -13,19 +13,43 @@ ROUTER.post('/', (req, res) => {
         res.status(500).send({ message: 'Internal Server Error' });
     })
 });
-
-
-
 // GET '/' - read all bounties
-
-
+ROUTER.get('/', (req, res) => {
+    DB.Bounty.find()
+    .then(bounties => {
+        res.status(200).send(bounties)
+    })
+    .catch(err => {
+        res.status(500).send({ message: 'Error hit at READ ALL BOUNTIES' })
+    })
+});
 // GET '/:id' - read one bounty
-
-
-
+ROUTER.get('/:id', (req, res) => {
+    DB.Bounty.findById(req.params.id)
+    .then(bounty => {
+        res.send(bounty)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(503).send({ message: 'Service unavailable '});
+    });
+});
 // PUT '/:id' - update one bounty
-
-
+ROUTER.put('/:id', (req, res) => {
+    console.log(req.body);
+    DB.Bounty.findByIdAndUpdate(
+        {_id: req.params.id},
+        req.body,
+        { new: true }
+    )
+    .then(updatedBounty => {
+        res.send(updatedBounty);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(503).send({ message: 'Server problems, who dis?' });
+    });
+});
 // DELETE '/:id' - delete one bounty
 
 
